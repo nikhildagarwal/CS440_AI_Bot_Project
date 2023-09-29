ecbfc03fe26f9c443d28138ea559bca51831be4d
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-09-29 04:54:36 -0400

    bot2 changes

--- main.py
+++ main.py
@@ -38,13 +38,13 @@ def save_the_ship_bot1(grid, bot_loc):
         if grid[i][j] == SAFETY_BUTTON:
             sol_path = curr_path
             break
-        if i + 1 < dim and grid[i + 1][j] != 1 and (i+1,j) != fire_start:
+        if i + 1 < dim and grid[i + 1][j] != 1 and (i + 1, j) != fire_start and (i+1, j) not in curr_path:
             clone_and_queue(curr_path, queue, (i + 1, j))
-        if i - 1 >= 0 and grid[i - 1][j] != 1 and (i-1,j) != fire_start:
+        if i - 1 >= 0 and grid[i - 1][j] != 1 and (i - 1, j) != fire_start and (i-1, j) not in curr_path:
             clone_and_queue(curr_path, queue, (i - 1, j))
-        if j + 1 < dim and grid[i][j + 1] != 1 and (i,j+1) != fire_start:
+        if j + 1 < dim and grid[i][j + 1] != 1 and (i, j + 1) != fire_start and (i, j+1) not in curr_path:
             clone_and_queue(curr_path, queue, (i, j + 1))
-        if j - 1 >= 0 and grid[i][j - 1] != 1 and (i,j-1) != fire_start:
+        if j - 1 >= 0 and grid[i][j - 1] != 1 and (i, j - 1) != fire_start and (i, j-1) not in curr_path:
             clone_and_queue(curr_path, queue, (i, j - 1))
     if sol_path is None:
         return False
@@ -62,11 +62,52 @@ def save_the_ship_bot1(grid, bot_loc):
     return True
 
 
+def save_the_ship_bot2(grid):
+    dim = s1.dim
+    signal = True
+    while signal:
+        bot_loc = s1.robot_loc
+        if bot_loc in s1.fire_loc:
+            return False
+        if bot_loc == s1.button_loc:
+            return True
+        queue = deque()
+        p = [bot_loc]
+        queue.append(p)
+        sol_path = None
+        while len(queue) > 0:
+            path = queue.popleft()
+            curr_loc = path[-1]
+            i = curr_loc[0]
+            j = curr_loc[1]
+            if grid[i][j] == SAFETY_BUTTON:
+                sol_path = path
+                break
+            if i + 1 < dim and grid[i + 1][j] != 1 and (i + 1, j) not in s1.fire_loc and (i+1, j) not in path:
+                clone_and_queue(path, queue, (i + 1, j))
+            if i - 1 >= 0 and grid[i - 1][j] != 1 and (i - 1, j) not in s1.fire_loc and (i-1, j) not in path:
+                clone_and_queue(path, queue, (i - 1, j))
+            if j + 1 < dim and grid[i][j + 1] != 1 and (i, j + 1) not in s1.fire_loc and (i, j+1) not in path:
+                clone_and_queue(path, queue, (i, j + 1))
+            if j - 1 >= 0 and grid[i][j - 1] != 1 and (i, j - 1) not in s1.fire_loc and (i, j-1) not in path:
+                clone_and_queue(path, queue, (i, j - 1))
+        if sol_path is None:
+            return False
+        sol_path.pop(0)
+        print(sol_path)
+        grid[s1.robot_loc[0]][s1.robot_loc[1]] = 0
+        s1.robot_loc = sol_path[0]
+        grid[s1.robot_loc[0]][s1.robot_loc[1]] = BOT_2
+        s1.spread_fire()
+        s1.print_out()
+        print("")
+
+
 def start_world(grid, bot, bot_loc):
     if bot == BOT_1:
         return save_the_ship_bot1(grid, bot_loc)
     elif bot == BOT_2:
-        return False
+        return save_the_ship_bot2(grid)
     elif bot == BOT_3:
         return False
     else:
@@ -78,12 +119,12 @@ if __name__ == '__main__':
     Change values here for dimension and type of bot
     """
     dimension = 5
-    bot = BOT_1
+    bot = BOT_2
     q = 0.9
     """"""
     if bot not in bot_set:
         raise TypeError("Bot must have one of the following values: -1, -2, -3, or -4")
-    s1 = Ship(dimension,q)
+    s1 = Ship(dimension, q)
     s1.init_environment(bot)
     s1.print_out()
     print(" ")
