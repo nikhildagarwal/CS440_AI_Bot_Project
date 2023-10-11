bf52c00a5a5aaabf3ff034fe4c1ff214254ec980
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-10-11 05:05:30 -0400

    A* search on bot2 and bo3

--- main.py
+++ main.py
@@ -51,6 +51,82 @@ def bot_1_path_Astar(s):
     return []
 
 
+def bot_2_path_Astar(s):
+    searchable = []
+    visited = {0}
+    start = ANode(s.bot_loc, 1, calculate_heuristic(s.bot_loc[0], s.button_loc[0], s.bot_loc[1], s.button_loc[1]), None)
+    key = {s.bot_loc: start}
+    heapq.heappush(searchable, start)
+    while searchable:
+        current_node = heapq.heappop(searchable)
+        key.pop(current_node.loc)
+        if current_node.loc == s.button_loc:
+            r_path = []
+            while current_node.prev is not None:
+                r_path.append(current_node.loc)
+                current_node = current_node.prev
+            return r_path
+        visited.add(current_node.loc)
+        i = current_node.loc[0]
+        j = current_node.loc[1]
+        neighbors = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
+        for neighbor in neighbors:
+            if (on_board(neighbor, s.dim) and neighbor not in visited and s.layout[neighbor[0]][neighbor[1]] != WALL
+                    and s.layout[neighbor[0]][neighbor[1]] != FIRE):
+                new_g = current_node.g + 1
+                new_h = calculate_heuristic(neighbor[0], s.button_loc[0], neighbor[1], s.button_loc[1])
+                new_node = ANode(neighbor, new_g, new_h, current_node)
+                get_node = key.get(neighbor, None)
+                if get_node is not None:
+                    if new_node.f < get_node.f:
+                        get_node.f = new_node.f
+                        get_node.g = new_node.g
+                        get_node.prev = current_node
+                        heapq.heapify(searchable)
+                else:
+                    heapq.heappush(searchable, new_node)
+                    key[neighbor] = new_node
+    return []
+
+
+def bot_3_path_Astar(s):
+    searchable = []
+    visited = {0}
+    start = ANode(s.bot_loc, 1, calculate_heuristic(s.bot_loc[0], s.button_loc[0], s.bot_loc[1], s.button_loc[1]), None)
+    key = {s.bot_loc: start}
+    heapq.heappush(searchable, start)
+    while searchable:
+        current_node = heapq.heappop(searchable)
+        key.pop(current_node.loc)
+        if current_node.loc == s.button_loc:
+            r_path = []
+            while current_node.prev is not None:
+                r_path.append(current_node.loc)
+                current_node = current_node.prev
+            return r_path
+        visited.add(current_node.loc)
+        i = current_node.loc[0]
+        j = current_node.loc[1]
+        neighbors = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
+        for neighbor in neighbors:
+            if (on_board(neighbor, s.dim) and neighbor not in visited and s.layout[neighbor[0]][neighbor[1]] != WALL
+                    and s.layout[neighbor[0]][neighbor[1]] != FIRE and neighbor not in s.fire_adj):
+                new_g = current_node.g + 1
+                new_h = calculate_heuristic(neighbor[0], s.button_loc[0], neighbor[1], s.button_loc[1])
+                new_node = ANode(neighbor, new_g, new_h, current_node)
+                get_node = key.get(neighbor, None)
+                if get_node is not None:
+                    if new_node.f < get_node.f:
+                        get_node.f = new_node.f
+                        get_node.g = new_node.g
+                        get_node.prev = current_node
+                        heapq.heapify(searchable)
+                else:
+                    heapq.heappush(searchable, new_node)
+                    key[neighbor] = new_node
+    return bot_2_path_Astar(s)
+
+
 def bot_1_path(s):
     fringe = Fringe(Node(s.bot_loc))
     bot_path = []
@@ -137,7 +213,7 @@ def bot_3_path(s):
 
 def runner():
     mod = 2
-    bot_list = [BOT_2]
+    bot_list = [BOT_1, BOT_2, BOT_3]
     q_values = []
     prob_values_bot_1 = []
     prob_values_bot_2 = []
@@ -167,22 +243,22 @@ def runner():
                     elif ship.bot == BOT_2:
                         success = True
                         while ship.bot_loc != ship.button_loc:
-                            path = bot_2_path(ship)
-                            ship.spread_fire()
+                            path = bot_2_path_Astar(ship)
                             if len(path) == 0:
                                 success = False
                                 break
+                            ship.spread_fire()
                             if path[-1] in ship.fire_loc:
                                 success = False
                                 break
-                            ship.bot_loc = path.pop(-1)
+                            ship.bot_loc = path[-1]
                         if success:
                             count += 1
                         total += 1
                     elif ship.bot == BOT_3:
                         success = True
                         while ship.bot_loc != ship.button_loc:
-                            path = bot_3_path(ship)
+                            path = bot_3_path_Astar(ship)
                             ship.spread_fire()
                             if len(path) == 0:
                                 success = False
@@ -203,13 +279,13 @@ def runner():
                 elif bot == BOT_3:
                     prob_values_bot_3.append(count/total)
                 print(bot,":",j/100,":",count/total)
-    print(len(prob_values_bot_3))
-    print(len(prob_values_bot_2))
-    print(len(prob_values_bot_1))
-    print(len(q_values))
-    #plt.plot(q_values, prob_values_bot_1, label='Bot 1', marker='.', linestyle='-', color='b')
+    with open('test_data.txt', 'w') as file:
+        file.write(str(prob_values_bot_1)+"\n")
+        file.write(str(prob_values_bot_2) + "\n")
+        file.write(str(prob_values_bot_3) + "\n")
+    plt.plot(q_values, prob_values_bot_1, label='Bot 1', marker='.', linestyle='-', color='b')
     plt.plot(q_values, prob_values_bot_2, label='Bot 2', marker='.', linestyle='-', color='r')
-    #plt.plot(q_values, prob_values_bot_3, label="Bot 3", marker='.', linestyle='-', color='g')
+    plt.plot(q_values, prob_values_bot_3, label="Bot 3", marker='.', linestyle='-', color='g')
     plt.xlabel('q values (flamability constant)')
     plt.ylabel('average probability of success (400 trials per q value)')
     plt.legend()
